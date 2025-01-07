7911ddbb47b510cd587967eccccf01e92023d1ee
GabrielCoh <9924@holbertonstudents.com>
2025-01-07 09:57:34 +0000

    started working on the non ineractive shell

---
+++ shell_no_interactive.c
@@ -0,0 +1,29 @@
+#include "simple_shell.h"
+
+/**
+ * shell_no_interactive - unix command line interpreter
+ * Return: void
+ */
+
+void shell_no_interactive(void)
+{
+	char *line;
+	char **args;
+	int status = -1;
+
+	do
+	{
+		line = read_stream();
+		args = split_line(line);
+		status = executs_args(args);
+
+		free(line);
+		free(args);
+
+		if (status >= 0)
+		{
+			exit(status);
+		}
+	}
+	while (status == -1);
+}
